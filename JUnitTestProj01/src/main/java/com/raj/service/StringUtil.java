09a77f64327c34afb078b2f26574286099ffcee1
Ravi kumar <Ravi kumar@DESKTOP-26DU0CT>
2025-11-20 22:55:48 +0530

    TL add getLength() method

--- JUnitTestProj01/src/main/java/com/raj/service/StringUtil.java
+++ JUnitTestProj01/src/main/java/com/raj/service/StringUtil.java
@@ -9,8 +9,8 @@ public class StringUtil {
 		return str.equalsIgnoreCase(revStr);
 	}
 	
-	
 	public int getLength(String str) {
-		return str.length();
+		int length = str.length();
+		return length;
 	}
 }
