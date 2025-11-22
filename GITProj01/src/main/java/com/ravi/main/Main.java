fddc13be04e7e8f5a3e8287651d7d02ee19fa99f
Ravi kumar <Ravi kumar@DESKTOP-26DU0CT>
2025-11-22 16:42:25 +0530

    TL created B2 branch

--- GITProj01/src/main/java/com/ravi/main/Main.java
+++ GITProj01/src/main/java/com/ravi/main/Main.java
@@ -1,5 +1,6 @@
 package com.ravi.main;
 
+import com.ravi.nb.NetBanking;
 import com.ravi.service.ArithmeticOperation;
 import com.ravi.upi.UPIPayment;
 
@@ -10,8 +11,17 @@ public class Main {
 	System.out.println("Result: "+ao.add(10, 20));
 	System.out.println("Result: "+ao.sub(20, 10));
 	
+	System.out.println("========================");
+	
 	UPIPayment upiPayment=new UPIPayment();
 	System.out.println(upiPayment.doUPIPayment(9471718299L, 5000.0));
 	System.out.println(upiPayment.doUPIPaymentByAccno(123456789L, 10000.0));
-}
+
+	System.out.println("=========================");
+	
+	NetBanking nb=new NetBanking();
+	String result=nb.transferMoney(123456789L, 987456123L, 20000.0);
+	System.out.println(result);
+	
+	}
 }
---
+++ GITProj01/src/main/java/com/ravi/nb/NetBanking.java
@@ -0,0 +1,8 @@
+package com.ravi.nb;
+
+public class NetBanking {
+
+	public String transferMoney(long srcAccno,long decAccno,double amount) {
+		return amount+" amount is transfered from "+srcAccno+" to "+decAccno;
+	}
+}
